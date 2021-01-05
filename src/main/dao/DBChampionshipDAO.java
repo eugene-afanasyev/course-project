d684e827230321c69fb297a7fe411077d343476b
dmzpp <zhilcdmitriy@gmail.com>
2021-01-06 01:46:06 +1000

    Add dao for championships

--- src/main/dao/DBChampionshipDAO.java
+++ src/main/dao/DBChampionshipDAO.java
@@ -1,6 +1,10 @@
 package main.dao;
 
 import main.models.Championship;
+import main.models.Discipline;
+import main.utils.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -8,26 +12,39 @@ public class DBChampionshipDAO implements ChampionshipDAO{
 
     @Override
     public Championship findById ( int id ) {
-        return null;
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Championship.class, id);
     }
 
     @Override
     public void save ( Championship arg ) {
-
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(arg);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void remove ( Championship arg ) {
-
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.remove(arg);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void update ( Championship arg ) {
-
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(arg);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public List<Championship> findAll ( ) {
-        return null;
+        List<Championship> championships = (List<Championship>)  HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery("From Championship").list();
+        return championships;
     }
 }
