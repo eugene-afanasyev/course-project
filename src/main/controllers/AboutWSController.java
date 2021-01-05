48b83b91c30d2063f8e2b134bece6c709151e826
eugene-afanasyev <whoaeugene@gmail.com>
2021-01-05 15:02:47 +0900

    AboutWorldSkills scene was made

--- src/main/controllers/AboutWSController.java
+++ src/main/controllers/AboutWSController.java
@@ -1,4 +1,29 @@
 package main.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AboutWSController {
+    @FXML
+    public BorderPane contentBorder;
+
+    public void toWorldSkillsHistory(MouseEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/Views/WorldSkillsHistory.fxml"));
+            Scene scene = new Scene(root, 920, 640);
+            Stage stage = (Stage) contentBorder.getScene().getWindow();
+            scene.getStylesheets().add(getClass().getResource("/stylesheets/style.css").toExternalForm());
+            stage.setScene(scene);
+            stage.setResizable(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
