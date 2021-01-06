fb0d9aff24131bdb8df4bdc3f8e9c9c66cebbfa9
Vanislavsky <serzh7214@mail.ru>
2021-01-06 18:14:14 +1000

    add eventTab in AboutKrasnodarRegion.fxml

--- src/main/controllers/AboutKrasnodarRegionController.java
+++ src/main/controllers/AboutKrasnodarRegionController.java
@@ -2,11 +2,9 @@ package main.controllers;
 
 import com.mchange.io.FileUtils;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -16,14 +14,6 @@ import java.io.File;
 import java.io.IOException;
 
 public class AboutKrasnodarRegionController {
-
-    class EventNode extends AnchorPane {
-        public EventNode() throws IOException {
-            FXMLLoader ev = FXMLLoader.load(getClass().getResource("res/Views/event.fxml"));
-            ev.load();
-            ev.setController(this);
-        }
-    }
 
     @FXML
     private TabPane iformationPane;
