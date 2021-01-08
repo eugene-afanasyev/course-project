5f8ffaf417b197da94f5804ed4575e6fe24117fb
eugene-afanasyev <whoaeugene@gmail.com>
2021-01-08 12:17:12 +0900

    added Authorization menu view and controller, removed some unused stuff

---
+++ src/main/controllers/AuthorizationMenuController.java
@@ -0,0 +1,24 @@
+package main.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AuthorizationMenuController {
+    @FXML
+    public TextField idNumberField;
+    @FXML
+    public PasswordField passwordField;
+    @FXML
+    public CheckBox rememberMeChBox;
+    @FXML
+    public Button loginButton;
+    @FXML
+    public Button cancelButton;
+
+    public void initialize() {
+        HeaderController.viewPath = "/Views/main.fxml";
+    }
+}
