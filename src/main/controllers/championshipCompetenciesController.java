bc1765cab0926c52adb218fe35461969c7bf0844
Vanislavsky <serzh7214@mail.ru>
2021-01-08 19:08:01 +1000

    add competencies in treeView in championshipCompetenciesController

--- src/main/controllers/championshipCompetenciesController.java
+++ src/main/controllers/championshipCompetenciesController.java
@@ -1,14 +1,24 @@
 package main.controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import main.dao.DBDisciplineDAO;
+import main.models.Discipline;
+import main.services.DisciplineService;
+
+import java.util.List;
 
 public class championshipCompetenciesController {
     @FXML
-    private TextArea informationArea;
+    private TextArea competenceInformationArea;
+
+    @FXML
+    private Label competenceNameLabel;
 
     @FXML
     private TreeView<Label> competences;
@@ -23,25 +33,47 @@ public class championshipCompetenciesController {
         return label;
     }
 
+    Discipline findDiscipline(List<Discipline> list, String name) {
+        for(var el : list) {
+            if(el.getName().equals(name))
+                return el;
+        }
+        return null;
+    }
+
     @FXML
     void initialize() {
+        competenceInformationArea.setWrapText(true);
+        competenceInformationArea.setStyle("");
+        competenceInformationArea.setEditable(false);
+
+        var disciplineService = new DisciplineService<>(DBDisciplineDAO::new);
+        var allDisciplines = disciplineService.findAll();
+        System.out.println(allDisciplines);
+
         TreeItem<Label> rootTreeNode = new TreeItem<Label>(buildLabel("Все компетенции", 150,20));
 
-        TreeItem<Label> serviceTransports = new TreeItem<Label>(buildLabel("Обслуживание гражданского транспорта",
-                300, 60));
-        TreeItem<Label> buildingSphere = new TreeItem<Label>(buildLabel("Специалисты строительной системы",
-                300, 60));
-        TreeItem<Label> industrialProduction = new TreeItem<Label>(buildLabel("Специалисты, занятые на промышленном производстве",
-                300, 70));
-        TreeItem<Label> informationAndCommunicationTechnologies = new TreeItem<Label>(buildLabel("Специалисты информационных и коммуникаационных технологий",
-                300, 70));
-        TreeItem<Label> creativityAndDesign = new TreeItem<Label>(buildLabel("Творчество и дизайн", 300, 20));
-        TreeItem<Label> serviceSector = new TreeItem<Label>(buildLabel("Сфера услуг", 300, 20));
-
-        rootTreeNode.getChildren().addAll(serviceTransports, buildingSphere, industrialProduction,
-                informationAndCommunicationTechnologies, creativityAndDesign, serviceSector);
+        for(var competence : allDisciplines) {
+            TreeItem<Label> item = new TreeItem<Label>(buildLabel(competence.getName(), 300, 20));
+            rootTreeNode.getChildren().addAll(item);
+        }
 
         competences.setRoot(rootTreeNode);
+        competences.getSelectionModel().selectedItemProperty().addListener( new ChangeListener() {
+
+            @Override
+            public void changed(ObservableValue observable, Object oldValue,
+                                Object newValue) {
+
+                TreeItem<Label> selectedItem = (TreeItem<Label>) newValue;
+                System.out.println("Selected Text : " + selectedItem.getValue());
+                var competence = findDiscipline(allDisciplines, selectedItem.getValue().getText());
+                competenceNameLabel.setText(competence.getRuName());
+                competenceInformationArea.setText(competence.getDescription());
+            }
+
+        });
+
         HeaderController.viewPath = "/Views/AboutWorldSkills.fxml";
     }
 }
