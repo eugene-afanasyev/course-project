ac47a42ae5660dfc712b22600b0c448579758c57
eugene-afanasyev <whoaeugene@gmail.com>
2021-01-09 15:31:07 +0900

    added expert menu view and controller, small fixes

---
+++ src/main/controllers/ExpertMenuController.java
@@ -0,0 +1,17 @@
+package main.controllers;
+
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class ExpertMenuController {
+    public Label welcomeTextLabel;
+
+    public void toDraw(MouseEvent event) {
+    }
+
+    public void toGrades(MouseEvent event) {
+    }
+
+    public void toResults(MouseEvent event) {
+    }
+}
