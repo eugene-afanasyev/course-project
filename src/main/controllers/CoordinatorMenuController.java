365e4dfa8399d4ee708bd475573891cf0ec447a9
eugene-afanasyev <whoaeugene@gmail.com>
2021-01-09 14:59:15 +0900

    added transition to Coordinator menu

---
+++ src/main/controllers/CoordinatorMenuController.java
@@ -0,0 +1,17 @@
+package main.controllers;
+
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class CoordinatorMenuController {
+    public Label welcomeTextLabel;
+
+    public void toManageVolunteers(MouseEvent event) {
+    }
+
+    public void toManageSponsors(MouseEvent event) {
+    }
+
+    public void toMyResults(MouseEvent event) {
+    }
+}
