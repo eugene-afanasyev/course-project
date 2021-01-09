0ef9d9969fa35678324ed2bfceaacc974714765f
eugene-afanasyev <whoaeugene@gmail.com>
2021-01-09 13:01:54 +0900

    added CompetitorMenu view and controller

---
+++ src/main/controllers/CompetitorMenuController.java
@@ -0,0 +1,19 @@
+package main.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class CompetitorMenuController {
+    @FXML
+    public Label welcomeTextLabel;
+
+    public void toMyProfile(MouseEvent event) {
+    }
+
+    public void toMyCompetencies(MouseEvent event) {
+    }
+
+    public void toMyResults(MouseEvent event) {
+    }
+}
