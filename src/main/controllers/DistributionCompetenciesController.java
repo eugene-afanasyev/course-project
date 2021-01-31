9471e1c6418a6e823ae54dd077bc523d46b2cf08
Vanislavsky <serzh7214@mail.ru>
2021-02-01 01:00:30 +1000

    add logic for second table in DistributionCompetenciesController

--- src/main/controllers/DistributionCompetenciesController.java
+++ src/main/controllers/DistributionCompetenciesController.java
@@ -6,9 +6,13 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import main.AuthManager;
 import main.dao.DBChampionshipDAO;
 import main.dao.DBDisciplineDAO;
+import main.models.Discipline;
+import main.models.User;
 import main.services.ChampionshipService;
 import main.services.DisciplineService;
 import tableModels.VoluunterCompetence;
+
+import java.util.List;
 
 public class DistributionCompetenciesController {
     @FXML
@@ -36,6 +40,45 @@ public class DistributionCompetenciesController {
     private Button searchButton;
 
     @FXML
+    private ComboBox<String> secondCompetenciesComboBox;
+
+    @FXML
+    private Button secondSearchButton;
+
+    @FXML
+    private TableView<VoluunterCompetence> secondVoluunterTable;
+
+    @FXML
+    private TableColumn<VoluunterCompetence, CheckBox> secondCheckColumn;
+
+    @FXML
+    private TableColumn<VoluunterCompetence, String> secondNameColumn;
+
+    @FXML
+    private TableColumn<VoluunterCompetence, String> secondSexColumn;
+
+    @FXML
+    private TableColumn<VoluunterCompetence, String> secondAgeColumn;
+
+    @FXML
+    private TableColumn<VoluunterCompetence, String> secondRegionColumn;
+
+    void fillComboBox(ComboBox<String> comboBox, List<Discipline> disciplines) {
+        for(var discipline: disciplines) {
+            comboBox.getItems().add(discipline.getName());
+        }
+        comboBox.setValue(disciplines.get(0).getName());
+    }
+
+    void fillTable(TableView<VoluunterCompetence> tableView, List<User> volunteers) {
+        for(var voluunter : volunteers) {
+            var sex = voluunter.isMale()? "male": "female";
+            tableView.getItems().add(new VoluunterCompetence(
+                    voluunter.getFirstName(), voluunter.getBirthdayDate().toString(), sex, voluunter.getRegion().getName()));
+        }
+    }
+
+    @FXML
     public void initialize() {
         checkColumn.setCellValueFactory(new PropertyValueFactory<VoluunterCompetence, CheckBox>("check"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<VoluunterCompetence, String>("name"));
@@ -49,24 +92,35 @@ public class DistributionCompetenciesController {
 
         var allDisciplines = disciplineService.findAll();
 
-        for(var discipline: allDisciplines) {
-            competenciesComboBox.getItems().add(discipline.getRuName());
-        }
-        competenciesComboBox.setValue(allDisciplines.get(0).getRuName());
+        fillComboBox(competenciesComboBox, allDisciplines);
+        fillComboBox(secondCompetenciesComboBox, allDisciplines);
+//
+//        for(var discipline: allDisciplines) {
+//            competenciesComboBox.getItems().add(discipline.getName());
+//        }
+//        competenciesComboBox.setValue(allDisciplines.get(0).getName());
 
         searchButton.setOnAction(actionEvent -> {
             var currentDiscipline = disciplineService.findByName(competenciesComboBox.getValue());
-
             var currentUser = AuthManager.Current.getUser();
-
             var volunteers = service.findAllByRole("Volunteer",
                     currentUser.getChampionship(), currentDiscipline);
 
-            for(var voluunter : volunteers) {
-                var sex = voluunter.isMale()? "male": "female";
-                voluunterTable.getItems().add(new VoluunterCompetence(
-                        voluunter.getFirstName(), voluunter.getBirthdayDate().toString(), sex, voluunter.getRegion().getName()));
-            }
+            fillTable(voluunterTable , volunteers);
+//            for(var voluunter : volunteers) {
+//                var sex = voluunter.isMale()? "male": "female";
+//                voluunterTable.getItems().add(new VoluunterCompetence(
+//                        voluunter.getFirstName(), voluunter.getBirthdayDate().toString(), sex, voluunter.getRegion().getName()));
+//            }
+        });
+
+        secondSearchButton.setOnAction(actionEvent -> {
+            var currentDiscipline = disciplineService.findByName(competenciesComboBox.getValue());
+            var currentUser = AuthManager.Current.getUser();
+            var volunteers = service.findAllByRole("Volunteer",
+                    currentUser.getChampionship(), currentDiscipline);
+
+            fillTable(secondVoluunterTable , volunteers);
         });
 
     }
