c5a012306fa08632a3d016ab0d538ff2e031fdc7
eugene-afanasyev <whoaeugene@gmail.com>
2021-02-03 12:50:15 +0900

    fixed style in AboutWSRussia view, added test files also

--- src/main/controllers/AboutWSRController.java
+++ src/main/controllers/AboutWSRController.java
@@ -1,13 +1,61 @@
 package main.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class AboutWSRController {
     @FXML
-    public BorderPane contentBorder;
+    public VBox contentBorder;
+    public VBox filesBox;
 
     public void initialize() {
         HeaderController.viewPath = "/Views/main.fxml";
+    }
+
+
+    public void showTestFiles1(MouseEvent event) {
+        filesBox.getChildren().clear();
+        for (int i = 1; i <= 3; i++) {
+            filesBox.getChildren().add(createLoadLink("Test file 1." + i));
+        }
+    }
+
+    public void showTestFiles2(MouseEvent event) {
+        filesBox.getChildren().clear();
+        for (int i = 1; i <= 4; i++) {
+            filesBox.getChildren().add(createLoadLink("Test file 2." + i));
+        }
+    }
+
+    public void showTestFiles3(MouseEvent event) {
+        filesBox.getChildren().clear();
+        for (int i = 1; i <= 7; i++) {
+            filesBox.getChildren().add(createLoadLink("Test file 3." + i));
+        }
+    }
+
+    private Label createLoadLink(String text) {
+        Label l = new Label(text);
+        l.setWrapText(true);
+        l.setOnMouseEntered((event -> {
+            l.setUnderline(true);
+        }));
+        l.setOnMouseExited((event -> {
+            l.setUnderline(false);
+        }));
+        l.setTextFill(Color.CORNFLOWERBLUE);
+        l.setStyle("-fx-font-size: 26; -fx-font-weight: bold");
+        l.setOnMouseClicked((event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.showSaveDialog(new Stage());
+        }));
+        return l;
     }
 }
